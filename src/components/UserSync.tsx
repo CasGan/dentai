4558aa5448cc517556c81a1c3a292c6abf6fa856
Cassandra Ganska <89425242+CasGan@users.noreply.github.com>
2025-10-14 18:57:49 -0700

    UserSync created to sync neon and db of users

---
+++ src/components/UserSync.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { syncUser } from "@/lib/actions/users";
+
+
+function UserSync() {
+    const { isSignedIn, isLoaded } = useUser(); 
+
+    useEffect(() => {
+        const handleUserSync = async () => {
+            if(isLoaded && isSignedIn){
+                try {
+                    await syncUser();
+                } catch (error) {
+                    console.log("Failed to Sync User", error);
+                } 
+            }
+        };
+        handleUserSync()
+    }, [isLoaded, isSignedIn]);
+    return null; 
+}
+
+export default UserSync
